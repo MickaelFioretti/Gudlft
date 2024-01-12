acb66ae6fb8b699a347d604421e31f8d5c3e530b
Mickael Fioretti <fiorettimickael1@gmail.com>
2024-01-12 10:05:50 +0000

    Test: Add tests for purchasing places with sufficient and insufficient points

---
+++ app/tests/test_bookingPlace.py
@@ -0,0 +1,47 @@
+from server import app, loadClubs, loadCompetitions
+import pytest
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_purchase_places_with_sufficient_points(client):
+    clubs = loadClubs()
+    competitions = loadCompetitions()
+    sufficient_points_club = clubs[0]  # "Simply Lift" avec 13 points
+    competition = competitions[0]  # "Spring Festival" avec 25 places disponibles
+
+    response = client.post(
+        "/purchasePlaces",
+        data={
+            "club": sufficient_points_club["name"],
+            "competition": competition["name"],
+            "places": "5",
+        },
+    )
+    assert response.status_code == 200  # OK
+    assert "Great-booking complete!" in response.data.decode()
+
+
+def test_purchase_places_with_insufficient_points(client):
+    clubs = loadClubs()
+    competitions = loadCompetitions()
+    insufficient_points_club = clubs[1]  # "Iron Temple" avec 4 points
+    competition = competitions[0]  # "Spring Festival"
+
+    response = client.post(
+        "/purchasePlaces",
+        data={
+            "club": insufficient_points_club["name"],
+            "competition": competition["name"],
+            "places": "5",  # Plus que les points disponibles
+        },
+    )
+    assert response.status_code == 200  # OK, mais avec message d'erreur
+    assert (
+        "You don&#39;t have enough points for this booking." in response.data.decode()
+    )
